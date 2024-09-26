3a9b01f5dacc2271eba409bb72c6f949b805f6d9
jhoan2 <johnhoang1994@gmail.com>
2024-09-26 09:54:41 -0700

    fix(sentry): added the beforeSend to stop sending every error

--- sentry.edge.config.ts
+++ sentry.edge.config.ts
@@ -13,4 +13,10 @@ Sentry.init({
 
   // Setting this option to true will print useful information to the console while you're setting up Sentry.
   debug: false,
+  beforeSend: (event) => {
+    if (process.env.NODE_ENV === 'development') {
+      return null
+    }
+    return event
+  }
 });
