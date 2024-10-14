22b430751c130fe337859808c11502213a0eb7b0
John Hoang <johnhoang1994@gmail.com>
2024-10-14 13:53:08 -0700

    feat(channelConversation): create the route handler in the app router api folder

---
+++ src/app/api/channelFeed/route.ts
@@ -0,0 +1,51 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const NEYNAR_API_KEY = process.env.NEYNAR_API_KEY;
+const NEYNAR_API_URL = 'https://api.neynar.com/v2/farcaster/feed/channels?channel_ids=dailylearning';
+
+async function fetchNeynarAPI(url: string) {
+  const response = await fetch(url, {
+    headers: {
+      'accept': 'application/json',
+      'api_key': NEYNAR_API_KEY as string
+    }
+  });
+
+  if (!response.ok) {
+    throw new Error(`Neynar API error: ${response.status} ${response.statusText}`);
+  }
+
+  return response.json();
+}
+
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const fid = searchParams.get('fid');
+  const cursor = searchParams.get('cursor');
+  console.log(fid)
+
+  if (!fid) {
+    return NextResponse.json({ error: 'fid is required' }, { status: 400 });
+  }
+
+  try {
+
+    let url = new URL(NEYNAR_API_URL);
+    url.searchParams.append('viewer_fid', fid);
+    url.searchParams.append('limit', '3');
+    url.searchParams.append('reply_depth', '2');
+    url.searchParams.append('include_chronological_parent_casts', 'false');
+    url.searchParams.append('type', 'hash');
+
+    if (cursor) {
+      url.searchParams.append('cursor', cursor);
+    }
+
+    const conversationResponse = await fetchNeynarAPI(url.toString());
+    console.log(conversationResponse)
+    return NextResponse.json(conversationResponse);
+  } catch (error) {
+    console.error('Error fetching event feed:', error);
+    return NextResponse.json({ error: 'An error occurred while fetching the event feed' }, { status: 500 });
+  }
+}
--- src/server/farcaster.ts
+++ src/server/farcaster.ts
@@ -16,20 +16,7 @@ export async function getChannelDetails(id: string) {
     }
 }
 
-export async function getChannelFeed(id: string) {
-    const options = {
-        method: 'GET',
-        headers: { accept: 'application/json', api_key: process.env.NEYNAR_API_KEY! }
-    };
 
-    try {
-        const response = await fetch(`https://api.neynar.com/v2/farcaster/feed/channels?channel_ids=${id}&with_recasts=true&with_replies=false&limit=6&should_moderate=false`, options);
-        const data = await response.json();
-        return data;
-    } catch (err) {
-        throw err;
-    }
-}
 
 export async function getNewMembers(id: string) {
     const options = {
