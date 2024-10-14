3fe9327dcba6a917f077f016f60ba6d20887f972
John Hoang <johnhoang1994@gmail.com>
2024-10-13 20:04:33 -0700

    fix(side nav): made the buttons smaller, added foreground and background styles, and collapsible.

--- src/app/SideNav.tsx
+++ src/app/SideNav.tsx
@@ -1,94 +1,70 @@
 'use client'
 
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link'
 import Image from 'next/image'
-import { Home, Folder, UserRound } from 'lucide-react'
+import { Home, Folder, UserRound, ChevronLeft, ChevronRight } from 'lucide-react'
 import { usePathname } from 'next/navigation'
 import clsx from 'clsx'
 import { ModeToggle } from './NextThemeButton'
-import { NeynarAuthButton } from "@neynar/react";
-
+import { NeynarAuthButton } from "@neynar/react"
+import { Button } from "~/components/ui/button"
+import { ScrollArea } from "~/components/ui/scroll-area"
 
 export default function SideNav() {
-    const pathname = usePathname()
-    return (
-        <nav id="sidebar-mini" className="flex flex-col translate-x-0 -translate-x-full p-2 h-full xl:w-[300px] transition-all duration-300 transform hidden z-[1]  border-r-2 rounded-r-xl border-gray-700 md:block md:translate-x-0 md:end-auto md:bottom-0">
-            <div className="flex flex-col justify-center items-center gap-y-2 py-4">
-                <Link href={'/'}>
-                    <div className="flex items-center text-lg space-x-2 mb-4">
-                        <Image src='/icon48.png' alt='idealite logo' width={64} height={64} />
-                        <p className='lg:block hidden text-3xl text-amber-400'>Idealite</p>
-                    </div>
-                </Link>
-                <div className="inline-block [--placement:right]">
-                    <Link href={'/home'}>
-                        <button
-                            title='Home'
-                            className={clsx(
-                                'w-16 lg:w-36 h-16 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent',
-                                'text-gray-400 hover:bg-gray-700 disabled:opacity-50 disabled:pointer-events-none',
-                                'focus:outline-none focus:ring-1 focus:ring-gray-600',
-                                {
-                                    'bg-gray-600': pathname.includes('/home'),
-                                    'bg-gray-800': !pathname.includes('/home')
-                                }
-                            )}
-                        >
-                            <div className='flex items-center space-x-2 text-lg'>
-                                <Home />
-                                <p className='lg:block hidden text-white'>Home</p>
-                            </div>
-                        </button>
-                    </Link>
-                </div>
-                <div className="inline-block [--placement:right]">
-                    <Link href={'/projects'}>
-                        <button
-                            title='Projects'
-                            className={clsx(
-                                'w-16 lg:w-36 h-16 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent',
-                                'text-gray-400 hover:bg-gray-700 disabled:opacity-50 disabled:pointer-events-none',
-                                'focus:outline-none focus:ring-1 focus:ring-gray-600',
-                                {
-                                    'bg-gray-600': pathname.includes('/projects'),
-                                    'bg-gray-800': !pathname.includes('/projects')
-                                }
-                            )}
-                        >
-                            <div className='flex items-center text-lg space-x-2'>
-                                <Folder />
-                                <p className='lg:block hidden text-white'>Projects</p>
-                            </div>
-                        </button>
-                    </Link>
-                </div>
-                <div className="inline-block [--placement:right]">
-                    <Link href={'/profile'}>
-                        <button
-                            title='Profile'
-                            className={clsx(
-                                'w-16 lg:w-36 h-16 inline-flex justify-center items-center gap-x-2 text-sm font-semibold rounded-full border border-transparent',
-                                'text-gray-400 hover:bg-gray-700 disabled:opacity-50 disabled:pointer-events-none',
-                                'focus:outline-none focus:ring-1 focus:ring-gray-600',
-                                {
-                                    'bg-gray-600': pathname.includes('/profile'),
-                                    'bg-gray-800': !pathname.includes('/profile')
-                                }
-                            )}
-                        >
-                            <div className='flex items-center space-x-2 text-lg'>
-                                <UserRound />
-                                <p className='lg:block hidden text-white'>Profile</p>
-                            </div>
-                        </button>
-                    </Link>
-                </div>
-            </div>
-            <div className='pt-2 flex justify-center items-center space-x-2'>
-                <NeynarAuthButton />
-                <ModeToggle />
-            </div>
+  const pathname = usePathname()
+  const [isCollapsed, setIsCollapsed] = useState(false)
+
+  const toggleSidebar = () => {
+    setIsCollapsed(!isCollapsed)
+  }
+
+  const menuItems = [
+    { icon: Home, label: "Home", href: "/home" },
+    { icon: Folder, label: "Projects", href: "/projects" },
+    { icon: UserRound, label: "Profile", href: "/profile" },
+  ]
+
+  return (
+    <nav className={`flex flex-col h-screen bg-background text-foreground transition-all duration-300 ${isCollapsed ? 'w-20' : 'w-64'} border-r-2 rounded-r-xl border-border`}>
+      <div className="flex items-center justify-between p-4 bg-background text-foreground border-b">
+        <Link href='/'>
+          <div className="flex items-center space-x-2">
+            <Image src='/icon48.png' alt='idealite logo' width={32} height={32} />
+            {!isCollapsed && <h1 className="text-xl font-semibold text-amber-400">Idealite</h1>}
+          </div>
+        </Link>
+        <Button variant="ghost" size="icon" onClick={toggleSidebar}>
+          {isCollapsed ? <ChevronRight className="h-5 w-5" /> : <ChevronLeft className="h-5 w-5" />}
+        </Button>
+      </div>
+      <ScrollArea className='flex flex-col justify-between h-full'>
+        <nav className="p-2 space-y-4">
+          {menuItems.map((item, index) => (
+            <Link href={item.href} key={index}>
+              <Button
+                variant="ghost"
+                className={clsx(
+                  'w-full',
+                  isCollapsed ? 'px-2' : 'px-4 justify-start',
+                  'bg-background hover:bg-gray-100 dark:hover:bg-gray-800',
+                  {
+                    'bg-gray-100 dark:bg-gray-800 text-foreground': pathname.includes(item.href),
+                    'text-foreground': !pathname.includes(item.href)
+                  }
+                )}
+              >
+                <item.icon className='h-6 w-6 mr-2' />
+                {!isCollapsed && <span>{item.label}</span>}
+              </Button>
+            </Link>
+          ))}
         </nav>
-    )
+      </ScrollArea>
+      <div className="p-4 border-t flex flex-col items-center space-y-4 bg-background text-foreground">
+        {!isCollapsed && <NeynarAuthButton />}
+        <ModeToggle />
+      </div>
+    </nav>
+  )
 }
