84d9c55bc528e9fc54ed4de6272506b478f43ff0
John Hoang <johnhoang1994@gmail.com>
2024-10-15 21:11:17 -0700

    fix(castCard api): cleaned up

--- src/app/api/castCard/route.ts
+++ src/app/api/castCard/route.ts
@@ -39,77 +39,104 @@ export async function GET(req: NextRequest) {
   }
 }
 export async function PATCH(req: NextRequest) {
-    const { reaction_type, signer_uuid, target, target_author_fid, idem } = await req.json();
+  const { reaction_type, signer_uuid, target, target_author_fid, idem } =
+    await req.json();
 
     if (!reaction_type || !signer_uuid || !target || !target_author_fid) {
-        return NextResponse.json({ error: 'Missing required fields in request body' }, { status: 400 });
+    return NextResponse.json(
+      { error: "Missing required fields in request body" },
+      { status: 400 },
+    );
     }
 
     try {
         const options = {
-            method: 'POST',
+      method: "POST",
             headers: {
-                accept: 'application/json',
+        accept: "application/json",
                 api_key: process.env.NEYNAR_API_KEY!,
-                'content-type': 'application/json'
+        "content-type": "application/json",
             },
             body: JSON.stringify({
                 reaction_type,
                 signer_uuid,
                 target,
                 target_author_fid,
-                idem
-            })
+        idem,
+      }),
         };
 
-        const response = await fetch('https://api.neynar.com/v2/farcaster/reaction', options);
+    const response = await fetch(
+      "https://api.neynar.com/v2/farcaster/reaction",
+      options,
+    );
         const data = await response.json();
 
         if (!response.ok) {
-            throw new Error(`HTTP error! status: ${response.status}, message: ${data.message || 'Unknown error'}`);
+      throw new Error(
+        `HTTP error! status: ${response.status}, message: ${data.message || "Unknown error"}`,
+      );
         }
 
         return NextResponse.json(data);
     } catch (err) {
         console.error(err);
-        return NextResponse.json({ error: 'An error occurred while fetching the event feed' }, { status: 500 });
+    return NextResponse.json(
+      { error: "An error occurred while fetching the event feed" },
+      { status: 500 },
+    );
     }
 }
 
 export async function DELETE(req: NextRequest) {
     const { searchParams } = new URL(req.url);
-    const eventCastHash = searchParams.get('eventCastHash');
+  const eventCastHash = searchParams.get("eventCastHash");
     const { signer_uuid } = await req.json();
 
     if (!signer_uuid) {
-        return NextResponse.json({ error: 'signer_uuid is required in the request body' }, { status: 400 });
+    return NextResponse.json(
+      { error: "signer_uuid is required in the request body" },
+      { status: 400 },
+    );
     }
 
     if (!eventCastHash) {
-        return NextResponse.json({ error: 'eventCastHash is required' }, { status: 400 });
+    return NextResponse.json(
+      { error: "eventCastHash is required" },
+      { status: 400 },
+    );
     }
 
     try {
         const options = {
-            method: 'DELETE',
+      method: "DELETE",
             headers: {
-                accept: 'application/json',
+        accept: "application/json",
                 api_key: process.env.NEYNAR_API_KEY!,
-                'content-type': 'application/json'
+        "content-type": "application/json",
             },
-            body: JSON.stringify({ signer_uuid, target_hash: eventCastHash })
+      body: JSON.stringify({ signer_uuid, target_hash: eventCastHash }),
         };
 
-        const response = await fetch('https://api.neynar.com/v2/farcaster/cast', options);
+    const response = await fetch(
+      "https://api.neynar.com/v2/farcaster/cast",
+      options,
+    );
         const data = await response.json();
 
         if (response.ok) {
-            return NextResponse.json({ message: 'Cast deleted successfully', data });
+      return NextResponse.json({ message: "Cast deleted successfully", data });
         } else {
-            return NextResponse.json({ error: 'Failed to delete cast', data }, { status: response.status });
+      return NextResponse.json(
+        { error: "Failed to delete cast", data },
+        { status: response.status },
+      );
         }
     } catch (err) {
         console.error(err);
-        return NextResponse.json({ error: 'An error occurred while deleting the cast' }, { status: 500 });
+    return NextResponse.json(
+      { error: "An error occurred while deleting the cast" },
+      { status: 500 },
+    );
     }
 }
