14857c07eaf0da8b9c7483fd08e72cb8b9cf13c2
John Hoang <johnhoang1994@gmail.com>
2024-10-18 15:57:19 -0700

    feat(session provider): added the session provider to get the session on the client side

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,7 @@ import SideNav from "~/app/SideNav";
 import { ThemeProvider } from "~/app/ThemeProvider";
 import NeynarProvider from "~/app/NeynarProvider";
 import { Toaster } from "~/components/ui/sonner";
+import { SessionProvider } from "next-auth/react";
 
 export default function RootLayout({
   children,
@@ -24,11 +25,13 @@ export default function RootLayout({
             disableTransitionOnChange
           >
             <NeynarProvider>
-              <div className="flex h-screen">
-                <SideNav />
-                <div className="flex-1 overflow-y-auto">{children}</div>
-                <Toaster />
-              </div>
+              <SessionProvider>
+                <div className="flex h-screen">
+                  <SideNav />
+                  <div className="flex-1 overflow-y-auto">{children}</div>
+                  <Toaster />
+                </div>
+              </SessionProvider>
             </NeynarProvider>
           </ThemeProvider>
         </PHProvider>
