9db313dfb5173fd75e21d36a91aa2d85a575f204
John Hoang <johnhoang1994@gmail.com>
2024-10-20 11:27:30 -0700

    feat(tags table)

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -5,12 +5,13 @@ import { sql } from "drizzle-orm";
 import {
   index,
   pgTableCreator,
-  serial,
   timestamp,
   varchar,
   uuid,
   integer,
-  text
+  text,
+  boolean,
+  AnyPgColumn,
 } from "drizzle-orm/pg-core";
 
 /**
@@ -21,23 +22,45 @@ import {
  */
 export const createTable = pgTableCreator((name) => `idealite_${name}`);
 
-export const users = createTable(
-	"user",
+export const users = createTable("user", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  fid: integer("fid"),
+  custody_address: varchar("custody_address", { length: 256 }),
+  username: varchar("username", { length: 256 }),
+  display_name: varchar("display_name", { length: 256 }),
+  pfp_url: varchar("pfp_url", { length: 256 }),
+  bio: text("bio"),
+  role: varchar("role", { length: 50 }).default("user").notNull(),
+  createdAt: timestamp("created_at", { withTimezone: true })
+    .default(sql`CURRENT_TIMESTAMP`)
+    .notNull(),
+  updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+    () => new Date(),
+  ),
+});
+
+export const tags = createTable(
+  "tag",
   {
-		id: uuid("id").primaryKey().defaultRandom(),
-		fid: integer("fid"),
-    custody_address: varchar("custody_address", { length: 256 }),
-    username: varchar("username", { length: 256 }),
-    display_name: varchar("display_name", { length: 256 }),
-    pfp_url: varchar("pfp_url", { length: 256 }),
-    bio: text("bio"),
-		role: varchar("role", { length: 50 }).default("user").notNull(),
-    createdAt: timestamp("created_at", { withTimezone: true })
+    id: uuid("id").defaultRandom().primaryKey(),
+    name: text("name")
+      .notNull()
+      .unique()
+      .$default(() => sql`lower(name)`),
+    parent_id: uuid("parent_id").references((): AnyPgColumn => tags.id),
+    created_at: timestamp("created_at", { withTimezone: true })
       .default(sql`CURRENT_TIMESTAMP`)
       .notNull(),
-    updatedAt: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+    updated_at: timestamp("updated_at", { withTimezone: true }).$onUpdate(
       () => new Date(),
     ),
+    deleted: boolean("deleted").default(false),
+  },
+  (table) => {
+    return {
+      name_idx: index("name_idx").on(table.name),
+      created_at_idx: index("created_at_idx").on(table.created_at),
+      deleted_idx: index("deleted_idx").on(table.deleted),
+    };
   },
 );
-
