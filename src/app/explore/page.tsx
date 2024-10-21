e621a64c950838965b2da667b1d4d3c4a4e6677d
John Hoang <johnhoang1994@gmail.com>
2024-10-21 15:32:57 -0700

    fix(explore): the page now has a getUserTags function

--- src/app/explore/page.tsx
+++ src/app/explore/page.tsx
@@ -1,7 +1,13 @@
 import ExploreState from "./(ExploreTagTree)/ExploreState";
 import { getTagWithChildren } from "~/server/tagQueries";
+import { getUserTags } from "~/server/usersTagsQueries";
+import { auth } from "~/app/auth";
 
 export default async function Explore() {
+  const session = await auth();
+  const userId = session?.user?.id;
+
   const tag = await getTagWithChildren("5a6fa43e-7d62-4e3e-bc46-d0bd9c7997a3");
-  return <ExploreState tag={tag} />;
+  const userTags = userId ? await getUserTags(userId) : [];
+  return <ExploreState tag={tag} userTags={userTags} />;
 }
