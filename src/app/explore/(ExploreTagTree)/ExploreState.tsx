9c3d9fe67858d617d719aca14e10d4d7a0251da9
John Hoang <johnhoang1994@gmail.com>
2024-10-21 15:34:10 -0700

    fix(explore): added a function to createTagTree and state to hold the flattened arrays

--- src/app/explore/(ExploreTagTree)/ExploreState.tsx
+++ src/app/explore/(ExploreTagTree)/ExploreState.tsx
@@ -1,29 +1,67 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useMemo } from "react";
 import ExploreTagTree from "./ExploreTagTree";
 import CirclePack from "./CirclePack";
-import { TreeNodeData } from "~/server/tagQueries";
+import { SelectTag } from "~/server/tagQueries";
+import { buildHierarchicalTree } from "./buildHierarchicalUserTagTree";
 
 interface ExploreStateProps {
-  tag: TreeNodeData;
+  tag: SelectTag[];
+  userTags: SelectTag[];
 }
 
-export default function ExploreState({ tag }: ExploreStateProps) {
-  const [tagTree, setTagTree] = useState<TreeNodeData | undefined>();
-  const [initialTagTree] = useState<TreeNodeData | undefined>();
+interface TagNode extends SelectTag {
+  children: TagNode[];
+  isInBoth: boolean;
+}
 
-  const hasChanges = JSON.stringify(tagTree) !== JSON.stringify(initialTagTree);
+function createTagTree(
+  rootTags: SelectTag[],
+  userTags: SelectTag[],
+): TagNode[] {
+  const userTagSet = new Set(userTags.map((tag) => tag.id));
+
+  function buildTree(
+    tags: SelectTag[],
+    parentId: string | null = null,
+  ): TagNode[] {
+    return tags
+      .filter((tag) => tag.parent_id === parentId)
+      .map((tag) => ({
+        ...tag,
+        children: buildTree(tags, tag.id),
+        isInBoth: userTagSet.has(tag.id),
+      }));
+  }
+
+  return buildTree(rootTags);
+}
+
+export default function ExploreState({ tag, userTags }: ExploreStateProps) {
+  const [flatRootTag] = useState<SelectTag[]>(tag);
+  const [flatUserTags, setFlatUserTags] = useState<SelectTag[]>(userTags);
+
+  const tagTree = useMemo(
+    () => createTagTree(flatRootTag, flatUserTags),
+    [flatRootTag, flatUserTags],
+  );
+
+  const userTagTree = useMemo(
+    () => buildHierarchicalTree(flatUserTags),
+    [flatUserTags],
+  );
+  console.log(userTagTree);
 
   return (
     <div className="flex">
-      <ExploreTagTree
-        tagTree={tagTree}
-        setTagTree={setTagTree}
-        hasChanges={hasChanges}
-      />
+      <ExploreTagTree tagTree={userTagTree} />
       <div className="flex-1">
-        <CirclePack tag={tag} tagTree={tagTree} setTagTree={setTagTree} />
+        <CirclePack
+          tagTree={tagTree[0]}
+          flatUserTags={flatUserTags}
+          setFlatUserTags={setFlatUserTags}
+        />
       </div>
     </div>
   );
