00eced71442e1c10b1434e476beeb44db2d1bb14
John Hoang <johnhoang1994@gmail.com>
2024-10-23 10:08:54 -0700

    feat(projects): added pages, user_pages, pags_tags schema

--- src/server/db/schema.ts
+++ src/server/db/schema.ts
@@ -59,9 +59,9 @@ export const tags = createTable(
   },
   (table) => {
     return {
-      name_idx: index("name_idx").on(table.name),
-      created_at_idx: index("created_at_idx").on(table.created_at),
-      deleted_idx: index("deleted_idx").on(table.deleted),
+      name_idx: index("tag_name_idx").on(table.name),
+      created_at_idx: index("tag_created_at_idx").on(table.created_at),
+      deleted_idx: index("tag_deleted_idx").on(table.deleted),
     };
   },
 );
@@ -82,8 +82,84 @@ export const users_tags = createTable(
   (table) => {
     return {
       pk: primaryKey({ columns: [table.user_id, table.tag_id] }),
-      user_id_idx: index("user_id_idx").on(table.user_id),
+      user_id_idx: index("users_tags_user_id_idx").on(table.user_id),
+      tag_id_idx: index("users_tags_tag_id_idx").on(table.tag_id),
+    };
+  },
+);
+
+export const pages = createTable(
+  "page",
+  {
+    id: uuid("id").defaultRandom().primaryKey(),
+    name: text("name")
+      .notNull()
+      .unique()
+      .$default(() => sql`lower(name)`),
+    title: text("title"),
+    content: text("content"),
+    created_at: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updated_at: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date(),
+    ),
+    deleted: boolean("deleted").default(false),
+  },
+  (table) => {
+    return {
+      name_idx: index("page_name_idx").on(table.name),
+      created_at_idx: index("page_created_at_idx").on(table.created_at),
+      deleted_idx: index("page_deleted_idx").on(table.deleted),
+    };
+  },
+);
+
+export const pages_tags = createTable(
+  "pages_tags",
+  {
+    page_id: uuid("page_id")
+      .notNull()
+      .references(() => pages.id),
+    tag_id: uuid("tag_id")
+      .notNull()
+      .references(() => tags.id),
+    created_at: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+  },
+  (table) => {
+    return {
+      pk: primaryKey({ columns: [table.page_id, table.tag_id] }),
+      page_id_idx: index("page_id_idx").on(table.page_id),
       tag_id_idx: index("tag_id_idx").on(table.tag_id),
+    };
+  },
+);
+
+export const users_pages = createTable(
+  "users_pages",
+  {
+    user_id: uuid("user_id")
+      .notNull()
+      .references(() => users.id),
+    page_id: uuid("page_id")
+      .notNull()
+      .references(() => pages.id),
+    role: varchar("role", { length: 50 }).default("owner").notNull(),
+    created_at: timestamp("created_at", { withTimezone: true })
+      .default(sql`CURRENT_TIMESTAMP`)
+      .notNull(),
+    updated_at: timestamp("updated_at", { withTimezone: true }).$onUpdate(
+      () => new Date(),
+    ),
+  },
+  (table) => {
+    return {
+      pk: primaryKey({ columns: [table.user_id, table.page_id] }),
+      user_id_idx: index("users_pages_user_id_idx").on(table.user_id),
+      page_id_idx: index("users_pages_page_id_idx").on(table.page_id),
+      role_idx: index("users_pages_role_idx").on(table.role),
     };
   },
 );
