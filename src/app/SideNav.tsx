899237c37f1f091388bfb987af18359ee30ad19e
John Hoang <johnhoang1994@gmail.com>
2024-10-23 18:40:44 -0700

    fix(side nav): made default closed

--- src/app/SideNav.tsx
+++ src/app/SideNav.tsx
@@ -20,7 +20,7 @@ import { ScrollArea } from "~/components/ui/scroll-area";
 
 export default function SideNav() {
   const pathname = usePathname();
-  const [isCollapsed, setIsCollapsed] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(true);
 
   const toggleSidebar = () => {
     setIsCollapsed(!isCollapsed);
