3f9f2e4f2b86976f039aa30ce51e0ba89ed6755b
John Hoang <johnhoang1994@gmail.com>
2024-10-24 13:05:06 -0700

    fix(projects): moved the usertagtree component to the layout

---
+++ src/app/projects/layout.tsx
@@ -0,0 +1,20 @@
+import UserTagTree from "./(UserTagTree)/UserTagTree";
+import { auth } from "~/app/auth";
+import { getUserTagTree } from "~/server/queries/usersTags";
+
+export default async function UserTagTreeLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const session = await auth();
+  const userId = session?.user?.id;
+  const userTagTree = userId ? await getUserTagTree(userId) : [];
+
+  return (
+    <div className="flex">
+      <UserTagTree userTagTree={userTagTree} />
+      {children}
+    </div>
+  );
+}
--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,11 +1,3 @@
-import UserTagTree from "./(UserTagTree)/UserTagTree";
-import { auth } from "~/app/auth";
-import { getUserTagTree } from "~/server/queries/usersTags";
-
-export default async function Projects() {
-  const session = await auth();
-  const userId = session?.user?.id;
-  const userTagTree = userId ? await getUserTagTree(userId) : [];
-
-  return <UserTagTree userTagTree={userTagTree} />;
+export default function Projects() {
+  return null;
 }
