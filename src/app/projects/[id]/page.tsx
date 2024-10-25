31f913befdf0e5898647212a787ec2cb6c42caa6
John Hoang <johnhoang1994@gmail.com>
2024-10-24 21:58:47 -0700

    fix(projects): return null for the page in the id folder

--- src/app/projects/[id]/page.tsx
+++ src/app/projects/[id]/page.tsx
@@ -1,5 +1,3 @@
-"use client";
-
-export default function ProjectPage({ params }: { params: { id: string } }) {
-  return <div>{params.id}</div>;
+export default function ProjectsPage() {
+  return null;
 }
