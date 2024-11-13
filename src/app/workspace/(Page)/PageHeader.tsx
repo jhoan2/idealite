b6d304259dc9a1b1735fd4a7eed28d9611f5be91
John Hoang <johnhoang1994@gmail.com>
2024-11-13 10:50:00 -0800

    feat(server tabs): creating the page header where bread crumbs and buttons go

---
+++ src/app/workspace/(Page)/PageHeader.tsx
@@ -0,0 +1,74 @@
+"use client";
+
+import { useState } from "react";
+import { CirclePlus } from "lucide-react";
+import { Info } from "lucide-react";
+import TagCrumbs from "~/app/workspace/(Page)/TagCrumbs";
+import { Button } from "~/components/ui/button";
+import { type Tag } from "~/server/db/schema";
+import { SidebarTrigger } from "~/components/ui/sidebar";
+import AddMetadata from "~/app/workspace/[pageId]/(AddResource)/AddMetadata";
+import { usePathname } from "next/navigation";
+import { TreeTag } from "~/server/queries/usersTags";
+import { Resource } from "~/server/queries/resource";
+import PageResourceInfo from "~/app/workspace/[pageId]/(ResourceInfo)/PageResourceInfo";
+
+interface PageHeaderProps {
+  tags: Tag[];
+  userTagTree: TreeTag[];
+  resources: Resource[];
+}
+
+export function PageHeader({ tags, userTagTree, resources }: PageHeaderProps) {
+  const [isMetadataOpen, setIsMetadataOpen] = useState(false);
+  const [isAddMetadataOpen, setIsAddMetadataOpen] = useState(false);
+  const pathname = usePathname();
+  const currentPageId = pathname.split("/projects/")[1];
+
+  return (
+    <div className="p-4">
+      <div className="flex items-center justify-between pb-4">
+        <div className="flex-1"></div>
+        <div className="flex w-4/5 flex-1 justify-center">
+          <TagCrumbs tags={tags} />
+        </div>
+        <div className="flex flex-1 items-center justify-end">
+          {isMetadataOpen && (
+            <>
+              <Button
+                variant="ghost"
+                onClick={() => setIsAddMetadataOpen(true)}
+                title="Add metadata"
+              >
+                <CirclePlus className="h-4 w-4" />
+              </Button>
+              <AddMetadata
+                isOpen={isAddMetadataOpen}
+                onOpenChange={setIsAddMetadataOpen}
+                pageId={currentPageId || ""}
+              />
+            </>
+          )}
+          <Button
+            variant="ghost"
+            onClick={() => setIsMetadataOpen(!isMetadataOpen)}
+            title="Metadata tab"
+          >
+            <Info className="h-4 w-4" aria-label="Metadata tab" />
+          </Button>
+          <SidebarTrigger />
+        </div>
+      </div>
+      <div>
+        {isMetadataOpen && (
+          <PageResourceInfo
+            resources={resources}
+            tags={tags}
+            userTagTree={userTagTree}
+            currentPageId={currentPageId || ""}
+          />
+        )}
+      </div>
+    </div>
+  );
+}
---
+++ src/app/workspace/[pageId]/layout.tsx
@@ -0,0 +1,32 @@
+import { getPageTagHierarchy } from "~/server/queries/page";
+import { PageHeader } from "~/app/workspace/(Page)/PageHeader";
+import { getUserTagTree } from "~/server/queries/usersTags";
+import { auth } from "~/app/auth";
+import { getResourcesForPage, Resource } from "~/server/queries/resource";
+
+export default async function WorkspacePageLayout({
+  children,
+  params,
+}: {
+  children: React.ReactNode;
+  params: { pageId: string };
+}) {
+  const tagHierarchy = await getPageTagHierarchy(params.pageId);
+  const session = await auth();
+  const userId = session?.user?.id;
+  const userTagTree = userId ? await getUserTagTree(userId) : [];
+  const resources = userId ? await getResourcesForPage(params.pageId) : [];
+
+  return (
+    <div className="flex h-full w-full flex-col">
+      <div className="w-full pb-4">
+        <PageHeader
+          tags={tagHierarchy.flat()}
+          userTagTree={userTagTree}
+          resources={resources as Resource[]}
+        />
+      </div>
+      <div className="w-full flex-1">{children}</div>
+    </div>
+  );
+}
