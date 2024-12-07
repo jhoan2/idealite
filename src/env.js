79baefbc20866bc54ffd30f6629e47cf2ca6f6cf
John Hoang <johnhoang1994@gmail.com>
2024-12-07 12:32:39 -0800

    feat(image gen ai): putting in the api key

--- src/env.js
+++ src/env.js
@@ -18,6 +18,7 @@ export const env = createEnv({
     APP_URL: z.string(),
     UPSTASH_REDIS_REST_URL: z.string(),
     UPSTASH_REDIS_REST_TOKEN: z.string(),
+    BLACK_FOREST_API_KEY: z.string(),
   },
 
   /**
@@ -50,6 +51,7 @@ export const env = createEnv({
     NEXT_PUBLIC_PINATA_GATEWAY: process.env.NEXT_PUBLIC_PINATA_GATEWAY,
     UPSTASH_REDIS_REST_URL: process.env.UPSTASH_REDIS_REST_URL,
     UPSTASH_REDIS_REST_TOKEN: process.env.UPSTASH_REDIS_REST_TOKEN,
+    BLACK_FOREST_API_KEY: process.env.BLACK_FOREST_API_KEY,
   },
   /**
    * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
