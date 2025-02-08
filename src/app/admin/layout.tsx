6b1b752d0e4476e2070612113ddae363b8a2cb4d
John Hoang <johnhoang1994@gmail.com>
2025-02-08 10:50:09 -0800

    feat(Auth): add role-based access control and extended user session properties

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,7 +1,16 @@
-export default function AdminLayout({
+import { redirect } from "next/navigation";
+import { auth } from "~/app/auth";
+
+export default async function AdminLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
+  const session = await auth();
+
+  if (session?.user?.role !== "admin") {
+    redirect("/home");
+  }
+
   return <div>{children}</div>;
 }
