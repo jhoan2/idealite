ef58aa95f82cbdc7769de070ed39ee701e4d56a9
John Hoang <johnhoang1994@gmail.com>
2025-02-13 13:21:42 -0800

    feat(Flashcards): add PostHog event tracking for questions and answers page view

--- src/app/play/flashcards/page.tsx
+++ src/app/play/flashcards/page.tsx
@@ -3,11 +3,18 @@ import FlashCards from "./flashcards";
 import { auth } from "~/app/auth";
 import { getUserPlayStats } from "~/server/queries/user";
 import NoCardsDue from "./NoCardsDue";
+import { trackEvent } from "~/lib/posthog/server";
+
 export default async function FlashcardsPage() {
   const session = await auth();
   if (!session?.user?.id) {
     throw new Error("Unauthorized");
   }
+
+  trackEvent(session.user.fid, "questions_and_answers_page_viewed", {
+    username: session.user.username,
+  });
+
   const flashcards = await createQuestionAndAnswer();
   const userPlayStats = await getUserPlayStats(session.user.id);
 
