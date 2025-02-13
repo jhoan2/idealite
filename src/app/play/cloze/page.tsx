12f195f4cc14bbb33147fd85d8f1a7f04b8aeda3
John Hoang <johnhoang1994@gmail.com>
2025-02-13 14:35:54 -0800

    feat(Cloze): add Farcaster Frame support and dynamic rendering for Cloze game

--- src/app/play/cloze/page.tsx
+++ src/app/play/cloze/page.tsx
@@ -1,24 +1,60 @@
 import { createClozeCards } from "~/server/actions/card";
-import FlashCards from "../flashcards/flashcards";
 import { getUserPlayStats } from "~/server/queries/user";
 import { auth } from "~/app/auth";
-import NoCardsDue from "../flashcards/NoCardsDue";
+import dynamic from "next/dynamic";
+import { Metadata } from "next";
+
+const ClozeFrame = dynamic(() => import("./ClozeFrame"), {
+  ssr: false,
+});
+
+const BASE_URL =
+  process.env.NEXT_PUBLIC_DEPLOYMENT_URL || process.env.VERCEL_URL;
+const domain = BASE_URL ? `https://${BASE_URL}` : "http://localhost:3000";
+const route = `${domain}/play/cloze`;
+const frame = {
+  version: "next",
+  imageUrl: `${route}/opengraph-image`,
+  button: {
+    title: "Fill in the blank",
+    action: {
+      type: "launch_frame",
+      name: "fill_in_the_blank",
+      url: route,
+      splashImageUrl:
+        "https://purple-defensive-anglerfish-674.mypinata.cloud/ipfs/bafkreidlqpger2bsx56loncfxllrhx3y3msugosybbd5gjqudmirehs7xy",
+      splashBackgroundColor: "#f7f7f7",
+    },
+  },
+};
+
+export const revalidate = 300;
+
+export async function generateMetadata(): Promise<Metadata> {
+  return {
+    title: "Fill in the blank",
+    openGraph: {
+      title: "Fill in the blank",
+      description: "idealite fill in the blank game",
+    },
+    other: {
+      "fc:frame": JSON.stringify(frame),
+    },
+  };
+}
 
 export default async function ClozePage() {
   const session = await auth();
   if (!session?.user?.id) {
-    throw new Error("Unauthorized");
+    return <div>Check out the channel frame at /idealite</div>;
   }
+
   const userPlayStats = await getUserPlayStats(session.user.id);
   const flashcards = await createClozeCards();
 
-  if (flashcards.length === 0) {
-    return <NoCardsDue />;
-  }
-
   return (
     <div className="flex min-h-screen flex-col items-center justify-center">
-      <FlashCards flashcards={flashcards} userPlayStats={userPlayStats} />
+      <ClozeFrame flashcards={flashcards} userPlayStats={userPlayStats} />
     </div>
   );
 }
