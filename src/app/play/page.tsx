f02ea20d30d8cbe9aa3eb65e8a316feb26138dc1
John Hoang <johnhoang1994@gmail.com>
2025-02-13 12:21:43 -0800

    feat(Play): add user authentication and play stats to PlayPage

--- src/app/play/page.tsx
+++ src/app/play/page.tsx
@@ -1,5 +1,12 @@
+import { getUserPlayStats } from "~/server/queries/user";
 import Games from "./Games";
+import { auth } from "../auth";
 
-export default function PlayPage() {
-  return <Games />;
+export default async function PlayPage() {
+  const session = await auth();
+  if (!session?.user?.id) {
+    throw new Error("Unauthorized");
+  }
+  const userPlayStats = await getUserPlayStats(session.user.id);
+  return <Games userPlayStats={userPlayStats} />;
 }
