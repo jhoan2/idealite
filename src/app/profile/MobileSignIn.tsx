b7ff936741565eed7a4878b2f4de868c8ce6a56f
John Hoang <johnhoang1994@gmail.com>
2025-02-16 15:46:41 -0800

    feat(MobileSignIn): Add frame detection for mobile sign-in component

--- src/app/profile/MobileSignIn.tsx
+++ src/app/profile/MobileSignIn.tsx
@@ -1,7 +1,22 @@
 "use client";
 import { NeynarAuthButton } from "@neynar/react";
+import { sdk } from "@farcaster/frame-sdk";
+import { useEffect, useState } from "react";
 
 export default function MobileSignIn() {
+  const [isInFrame, setIsInFrame] = useState(false);
+  useEffect(() => {
+    const load = async () => {
+      const result = await sdk.actions.ready();
+      setIsInFrame(result !== undefined);
+    };
+    load();
+  }, []);
+
+  if (!isInFrame) {
+    return null;
+  }
+
   return (
     <div>
       <NeynarAuthButton />
