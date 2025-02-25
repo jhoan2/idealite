a4f43ee3ab1dc5ed02199115d19f9ba493dec636
John Hoang <johnhoang1994@gmail.com>
2025-02-24 19:50:26 -0800

    feat(GuessThePicture): Add game page, hosting, and supporting components for Guess the Picture game

---
+++ src/app/play/guess-the-picture/GuessPictureFrame.tsx
@@ -0,0 +1,29 @@
+"use client";
+
+import { sdk } from "@farcaster/frame-sdk";
+import { useEffect, useState } from "react";
+import GuessPicture from "./GuessPicture";
+export default function SpinWheelFrame() {
+  const [isSDKLoaded, setIsSDKLoaded] = useState(false);
+
+  useEffect(() => {
+    const load = async () => {
+      sdk.actions.ready();
+    };
+
+    if (sdk && !isSDKLoaded) {
+      setIsSDKLoaded(true);
+      load();
+    }
+  }, [isSDKLoaded]);
+
+  if (!isSDKLoaded) {
+    return <div>Loading...</div>;
+  }
+
+  return (
+    <div className="flex min-h-screen flex-col items-center justify-center">
+      <GuessPicture />
+    </div>
+  );
+}
