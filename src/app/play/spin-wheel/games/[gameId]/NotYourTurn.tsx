0996e46e39d1903523d19eb6c4bba2f87916ed20
John Hoang <johnhoang1994@gmail.com>
2025-02-26 10:57:54 -0800

    refactor(SpinWheel): Update NotYourTurn component for Spin the Wheel game

--- src/app/play/spin-wheel/games/[gameId]/NotYourTurn.tsx
+++ src/app/play/spin-wheel/games/[gameId]/NotYourTurn.tsx
@@ -2,21 +2,21 @@ import { Button } from "~/components/ui/button";
 import Link from "next/link";
 export default function NotYourTurn() {
   return (
-    <div className="h-[100dvh] w-full bg-[#CC412F]">
+    <div className="h-[100dvh] w-full bg-[#33A33C]">
       <div className="flex h-full w-full flex-col items-center justify-center">
         <div className="flex flex-col items-center justify-center gap-6">
-          <h1 className="text-2xl font-bold text-white">Friend Clash</h1>
+          <h1 className="text-2xl font-bold text-white">Spin the Wheel</h1>
           <h2 className="text-lg font-bold text-white">It's not your turn!</h2>
           <img
-            src="/games/friend-clash.png"
-            alt="Friend Clash"
+            src="/games/spin-the-wheel.png"
+            alt="Spin Wheel"
             className="h-48 w-48 object-contain"
             width={192}
             height={192}
           />
           <div className="flex w-48 flex-col gap-4">
             <Button variant="secondary" className="w-full">
-              <Link href="/play/friend-clash/games">Your Games</Link>
+              <Link href="/play/spin-wheel/games">Your Games</Link>
             </Button>
           </div>
         </div>
