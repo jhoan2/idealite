6d73a3db26c92313f4d9d00349ebc6e24deff98f
John Hoang <johnhoang1994@gmail.com>
2025-03-10 12:51:10 -0700

    feat(GlobalTagsTour): Add mobile-specific onboarding tour for global tags

--- src/app/workspace/global-tags/page.tsx
+++ src/app/workspace/global-tags/page.tsx
@@ -3,6 +3,8 @@ import { getTagWithChildren } from "~/server/queries/tag";
 import { getUserTags } from "~/server/queries/usersTags";
 import { auth } from "~/app/auth";
 import { GlobalTagsTour } from "./GlobalTagsTour";
+import { MobileGlobalTagsTour } from "./MobileGlobalTagsTour";
+import { headers } from "next/headers";
 
 export default async function GlobalTagsPage() {
   const session = await auth();
@@ -11,9 +13,29 @@ export default async function GlobalTagsPage() {
     process.env.NEXT_PUBLIC_ROOT_TAG_ID ?? "",
   );
   const userTags = await getUserTags(userId ?? "");
+  const headersList = headers();
+  const userAgent = headersList.get("user-agent");
+
+  const isMobile = userAgent?.toLowerCase().includes("mobile");
   return (
-    <GlobalTagsTour>
-      <GlobalTagState tag={tag} userTags={userTags} userId={userId ?? null} />
-    </GlobalTagsTour>
+    <>
+      {isMobile ? (
+        <MobileGlobalTagsTour>
+          <GlobalTagState
+            tag={tag}
+            userTags={userTags}
+            userId={userId ?? null}
+          />
+        </MobileGlobalTagsTour>
+      ) : (
+        <GlobalTagsTour>
+          <GlobalTagState
+            tag={tag}
+            userTags={userTags}
+            userId={userId ?? null}
+          />
+        </GlobalTagsTour>
+      )}
+    </>
   );
 }
