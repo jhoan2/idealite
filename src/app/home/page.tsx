9cb1fa725be8771bba5b00c08f894de2721d4877
John Hoang <johnhoang1994@gmail.com>
2025-03-11 10:21:13 -0700

    refactor: changing home from chat into dashboard

--- src/app/home/page.tsx
+++ src/app/home/page.tsx
@@ -1,13 +1,7 @@
-import ChannelFeed from "./(ChannelFeed)/ChannelFeed";
-import { Suspense } from "react";
-import LoadingFeed from "./(ChannelFeed)/LoadingFeed";
-
 export default function Home() {
   return (
-    <>
-      <Suspense fallback={<LoadingFeed />}>
-        <ChannelFeed />
-      </Suspense>
-    </>
+    <div>
+      <h1>Home</h1>
+    </div>
   );
 }
