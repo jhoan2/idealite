aa94f6183cab6ee8d85847d2dc77ce104607357c
John Hoang <johnhoang1994@gmail.com>
2025-03-15 21:02:26 -0700

    refactor(Environment): Replace DEPLOYMENT_URL with VERCEL_URL in environment configuration for consistency

--- src/env.js
+++ src/env.js
@@ -34,7 +34,7 @@ export const env = createEnv({
     RESEND_API_KEY: z.string(),
     RESEND_REPLY_TO_EMAIL: z.string(),
     RESEND_FROM_EMAIL: z.string(),
-    DEPLOYMENT_URL: z.string(),
+    VERCEL_URL: z.string(),
   },
 
   /**
@@ -91,7 +91,7 @@ export const env = createEnv({
     RESEND_API_KEY: process.env.RESEND_API_KEY,
     RESEND_REPLY_TO_EMAIL: process.env.RESEND_REPLY_TO_EMAIL,
     RESEND_FROM_EMAIL: process.env.RESEND_FROM_EMAIL,
-    DEPLOYMENT_URL: process.env.DEPLOYMENT_URL,
+    VERCEL_URL: process.env.VERCEL_URL,
   },
   /**
 
