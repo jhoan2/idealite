76c8a71b64f461a50bdcb5931e47cdcb02163bad
John Hoang <johnhoang1994@gmail.com>
2025-03-30 19:01:33 -0700

    fix(middleware): Update authorizedParties URL from idealite.xyz to www.idealite.xyz for consistency

--- src/middleware.ts
+++ src/middleware.ts
@@ -19,7 +19,7 @@ export default clerkMiddleware(
     }
   },
   {
-    authorizedParties: ["https://idealite.xyz", "http://localhost:3000"],
+    authorizedParties: ["https://www.idealite.xyz", "http://localhost:3000"],
   },
 );
 
