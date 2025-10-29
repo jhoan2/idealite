4fc670377e03734111abdff0341b81fc1ab19325
John Hoang <johnhoang1994@gmail.com>
2025-10-29 15:22:12 -0700

    feat(googletags): added google analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,10 +3,12 @@ import Progress from "./(landing)/Progress";
 import Footer from "./(landing)/Footer";
 import GetStarted from "./(landing)/GetStarted";
 import FeatureSection from "./(landing)/FeatureSection";
+import { GoogleTag } from "~/components/GoogleTag";
 
 export default function FrontPage() {
   return (
     <div className="min-h-screen bg-black text-gray-800">
+      <GoogleTag />
       <Hero />
       <main>
         <FeatureSection />
