a9483d1b67aa0221c96376d994e4a04d7b31f88f
John Hoang <johnhoang1994@gmail.com>
2025-11-13 10:58:22 -0800

    fix(image url): changed to assets.idealite

--- src/app/review/FlashcardReview.tsx
+++ src/app/review/FlashcardReview.tsx
@@ -166,7 +166,7 @@ export default function FlashcardReview({
         <div className="flex flex-col items-center">
           <div className="mb-4 flex max-h-[300px] w-full justify-center">
             <img
-              src={`https://idealite.xyz/${currentCard.image_cid}`}
+              src={`https://assets.idealite.xyz/${currentCard.image_cid}`}
               alt="Card image"
               className="max-h-[300px] rounded-md object-contain"
             />
