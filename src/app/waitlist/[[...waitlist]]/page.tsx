4b91718edf925016abb8e70f64aeee2538da5eac
John Hoang <johnhoang1994@gmail.com>
2025-11-19 22:50:41 -0800

    fix(googleads): added google tags to waitlist page

--- src/app/waitlist/[[...waitlist]]/page.tsx
+++ src/app/waitlist/[[...waitlist]]/page.tsx
@@ -1,9 +1,11 @@
 // src/app/waitlist/[[...waitlist]]/page.tsx
 import { Waitlist } from "@clerk/nextjs";
+import { GoogleTag } from "~/components/GoogleTag";
 
 export default function WaitlistPage() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-gray-50">
+      <GoogleTag />
       <Waitlist />
     </div>
   );
